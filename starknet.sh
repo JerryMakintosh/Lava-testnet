7713e5f24df38ba525002b33515da976946f5c1d
JerryMakintosh <jerrymakintosh2@gmail.com>
2024-06-11 01:20:38 +0200

    Update starknet.sh

--- starknet.sh
+++ starknet.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-RPC_URL="https://rpc.starknet.lava.build/lava-referer-00c6ad4a-5ae1-4fcd-b0ab-703adcd0635e/"
+RPC_URL="https://rpc.starknet.lava.build/lava-referer-440ef9d2-3fe8-4eb4-9c06-dc6f1b0bc850/"
 
 OUTPUT_DIR="logs"
 
